5e3a8af6fd419a01ad8201e653a39b7c1f1e4ebd
{Agata Kuklińska} <delicadency@gmail.com>
2025-12-31 16:35:13 +0100

    fix: update sitemap URLs to use the correct domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 export default function robots() {
   return {
     rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://k2.inked.vercel.app/sitemap.xml",
+    sitemap: "https://www.k2inked.pl/sitemap.xml",
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,19 +2,19 @@ import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: "https://k2.inked.vercel.app/", lastModified: new Date() },
-    { url: "https://k2.inked.vercel.app/galeria", lastModified: new Date() },
-    { url: "https://k2.inked.vercel.app/kontakt", lastModified: new Date() },
-    { url: "https://k2.inked.vercel.app/regulamin", lastModified: new Date() },
+    { url: "https://k2inked.pl/", lastModified: new Date() },
+    { url: "https://k2inked.pl/galeria", lastModified: new Date() },
+    { url: "https://k2inked.pl/kontakt", lastModified: new Date() },
+    { url: "https://k2inked.pl/regulamin", lastModified: new Date() },
     {
-      url: "https://k2.inked.vercel.app/polityka-prywatnosci",
+      url: "https://k2inked.pl/polityka-prywatnosci",
       lastModified: new Date(),
     },
-    { url: "https://k2.inked.vercel.app/klaudia", lastModified: new Date() },
-    { url: "https://k2.inked.vercel.app/kari", lastModified: new Date() },
-    { url: "https://k2.inked.vercel.app/sonia", lastModified: new Date() },
-    { url: "https://k2.inked.vercel.app/ewelina", lastModified: new Date() },
-    { url: "https://k2.inked.vercel.app/mirella", lastModified: new Date() },
-    { url: "https://k2.inked.vercel.app/emi", lastModified: new Date() },
+    { url: "https://k2inked.pl/klaudia", lastModified: new Date() },
+    { url: "https://k2inked.pl/kari", lastModified: new Date() },
+    { url: "https://k2inked.pl/sonia", lastModified: new Date() },
+    { url: "https://k2inked.pl/ewelina", lastModified: new Date() },
+    { url: "https://k2inked.pl/mirella", lastModified: new Date() },
+    { url: "https://k2inked.pl/emi", lastModified: new Date() },
   ];
 }
